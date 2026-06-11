8b1da7f3bbb7b65edc8a7cc25cd59da7e5109d35
BeesBarGirl25 <beesbargirl25@gmail.com>
2026-06-11 16:15:50 +0100

    Add Mark Williams to seed data with entry fee

--- seed_data.py
+++ seed_data.py
@@ -755,7 +755,8 @@ PARTICIPANTS = [
     {"name": "Raphael Zindi",      "entry_fee_paid": 10.00},  # 2 entries
     {"name": "Gemma Ullyatt",      "entry_fee_paid":  5.00},  # 1 entry
     {"name": "William Waterton",   "entry_fee_paid": 10.00},  # 2 entries
-    {"name": "Nicholas Barradas",  "entry_fee_paid":  5.00}  # 1 entry
+    {"name": "Nicholas Barradas",  "entry_fee_paid":  5.00},  # 1 entry
+    {"name": "Mark Williams",      "entry_fee_paid": 10.00}   # 2 entries
 ]
 
 FUN_CATEGORIES = [
