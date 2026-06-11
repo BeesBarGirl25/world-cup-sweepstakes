5eb8ff63798d0ddf0a52fcc2a7fa2cbdd142ea9c
BeesBarGirl25 <beesbargirl25@gmail.com>
2026-06-11 16:32:27 +0100

    Update seed_data.py

--- seed_data.py
+++ seed_data.py
@@ -756,7 +756,8 @@ PARTICIPANTS = [
     {"name": "Gemma Ullyatt",      "entry_fee_paid":  5.00},  # 1 entry
     {"name": "William Waterton",   "entry_fee_paid": 10.00},  # 2 entries
     {"name": "Nicholas Barradas",  "entry_fee_paid":  5.00},  # 1 entry
-    {"name": "Mark Williams",      "entry_fee_paid": 10.00}   # 2 entries
+    {"name": "Mark Williams",      "entry_fee_paid": 10.00},   # 2 entries
+    {"name": "Paul Lamb",          "entry_fee_paid": 10.00}   # 2 entries
 ]
 
 FUN_CATEGORIES = [
